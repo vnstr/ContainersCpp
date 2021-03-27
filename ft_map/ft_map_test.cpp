2540a242c04bf8dd8d33fe483b5eb783e6684c25
Ivan Vostrilov <vnstr.main@gmail.com>
2021-03-27 20:40:26 +0300

    ft_map: release ft_map.hpp 1.0

--- ft_map/ft_map_test.cpp
+++ ft_map/ft_map_test.cpp
@@ -395,20 +395,3 @@ int main() {
 	// ------------
 	return 0;
 }
-//int main ()
-//{
-//	ft::Map<char,int> mymap;
-//	ft::Map<char,int>::iterator itlow,itup;
-//	mymap['a']=20;
-//	mymap['b']=40;
-//	mymap['c']=60;
-//	mymap['d']=80;
-//	mymap['e']=100;
-//	itlow=mymap.lower_bound ('b');  // itlow points to b
-//	itup=mymap.upper_bound ('d');   // itup points to e (not d!)
-//	mymap.erase(itlow,itup);        // erases [itlow,itup)
-//	// print content:
-//	for (ft::Map<char,int>::iterator it=mymap.begin(); it!=mymap.end(); ++it)
-//		std::cout << it->first << " => " << it->second << '\n';
-//	return 0;
-//}
